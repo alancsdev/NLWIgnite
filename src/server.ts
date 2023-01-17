863d43884c61ac9edc7f9878138a58406be45477
Alan Santos <alanchs@live.com>
2023-01-16 21:52:53 -0300

    Server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,21 @@
 //
 
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return 'Hello World!';
+app.register(cors);
+
+app.get('/', async () => {
+  const habits = await prisma.habit.findMany({
+    where: { title: { startsWith: 'Beber' } },
+  });
+  return habits;
 });
 
-app.listen({ port: 3000 });
+app.listen({ port: 3000 }).then(() => {
+  console.log('HTTP Server running!');
+});
